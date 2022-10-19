886d20ff2f6238f6cc20ff1157b22f5eb5616d19
atkindc <david.atkinsin@netzero.com>
2022-10-18 21:34:00 -0500

    telop and lift update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleopLiftAndGripper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleopLiftAndGripper.java
@@ -25,7 +25,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @Config
 @TeleOp(group = "Test")
-@Disabled
+//@Disabled
 public class TeleopLiftAndGripper<slideTrainerState> extends LinearOpMode {
 
     ElapsedTime runtime = new ElapsedTime();
