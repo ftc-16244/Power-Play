42a9809ffeeb3e28898da187d8d30d75e27caa0b
David Atkinon <davidatkinson549@gmail.com>
2022-10-20 20:54:24 -0500

    uodated meep meep for power play auto

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingTest.java
@@ -0,0 +1,41 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTestingTest {
+    public static double DISTANCE = 50;
+    public static double RIGHTDISTANCE = 11;
+    public static void main(String[] args) {
+
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        // Declare our first bot
+        RoadRunnerBotEntity myFirstBot = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be blue
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 13)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(60, 35, Math.toRadians(180)))
+                                .forward(DISTANCE)
+                                .turn(Math.toRadians(235))
+                                .build()
+                );
+
+
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+
+                // Add both of our declared bot entities
+                .addEntity(myFirstBot)
+                .start();
+    }
+}
